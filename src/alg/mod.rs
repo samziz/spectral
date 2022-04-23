593ecff4bbb38a24acdaed6192a75728efdab6e1
Sam Robinson-Adams <samziz@me.com>
2022-04-23 02:05:55 +0100

    document `alg` (algebra) mod

--- src/alg/mod.rs
+++ src/alg/mod.rs
@@ -1,1 +1,7 @@
-pub mod mul;
+//! This module reexports all the trait impls in submodules such
+//! as [`arith`]. It is concerned with the core linear algebraic
+//! operations.
+
+mod arith;
+
+pub use arith::*;
